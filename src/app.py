7373850542204d6c45f64438ae824a2fd11f2f4d
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-09-15 16:00:58 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,51 +1,10 @@
 import asyncio
 
+from contextvars import ContextVar
+
+from time import sleep
+
+import requests
 
 
-
-async def count(counter):
-    
-    print(F'Количество записей в списке: {len(counter)}')
-    
-    while True:
-        await asyncio.sleep(1 / 1000)
-        counter.append(1)
-        
-        
-async def print_every_one_sec(counter):
-
-    while True:
-        await asyncio.sleep(1)
-        print(F'- 1 секунда прошла. Количество записей в списке: {len(counter)}')
-        
-        
-async def print_every_5_sec():
-
-    while True:
-        await asyncio.sleep(5)
-        print(F'---- 5 секунд прошло.')
-        
-        
-async def print_every_10_sec():
-
-    while True:
-        await asyncio.sleep(10)
-        print(F'-------- 10 секунд прошло.')
-        
-        
-async def main():
-
-    counter = list()
-    
-    c = count(counter)
-    p1 = print_every_one_sec(counter)
-    p5 = print_every_5_sec()
-    p10 = print_every_10_sec()
-    
-    asyncio.create_task(c)
-    asyncio.create_task(p1)
-    asyncio.create_task(p5)
-    await p10
-    
-    
-asyncio.run(main())
+requests
